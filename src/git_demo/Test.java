2979250e46bbb4ef7a08aa562551b96dc1c38ae0
GitHub <taianjrb@126.com>
2016-03-21 13:44:18 +0800

    又增加了一个测试类

--- src/git_demo/Test.java
+++ src/git_demo/Test.java
@@ -3,5 +3,8 @@ package git_demo;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World£¡");
+        
+        System.out.println("GitHub !");
+        
     }
 }
---
+++ src/git_demo/Test2.java
@@ -0,0 +1,7 @@
+package git_demo;
+
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("增加一个测试类！");
+    }
+}
